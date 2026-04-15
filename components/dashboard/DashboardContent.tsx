0ef83b619d5d88cd419f72901a8f7646c43aaefd
zetomis <zetomis.was.taken@gmail.com>
2026-04-15 22:36:12 +0700

    feat: add basic styling for dashboard content and time telling

--- components/dashboard/DashboardContent.tsx
+++ components/dashboard/DashboardContent.tsx
@@ -1,10 +1,61 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { colors } from "@/theme/colors";
+import { getDimensionTier } from "@/theme/dimension";
+import { globalStyles } from "@/theme/globalStyles";
+import { typography } from "@/theme/typography";
+import React, { useEffect, useState } from "react";
+import { Text, useWindowDimensions, View } from "react-native";
 
 const DashboardContent = () => {
+  const { width } = useWindowDimensions();
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const isSmall = getDimensionTier(width) == "small";
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <View>
-      <Text>DashboardContent</Text>
+    <View
+      style={[
+        globalStyles.card,
+        {
+          borderColor: colors.textPrimary,
+          borderWidth: 2,
+          marginHorizontal: isSmall ? 32 : 64,
+          flexDirection: "column",
+          columnGap: 4,
+        },
+      ]}
+    >
+      <View
+        style={[
+          {
+            flexDirection: "column",
+            columnGap: 2,
+          },
+        ]}
+      >
+        <Text style={[typography.bodyLarge, { fontWeight: "semibold" }]}>
+          {currentDate.toLocaleTimeString("en-US", {
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
+        </Text>
+        <Text
+          style={[typography.textTertiary, { color: colors.textSecondary }]}
+        >
+          {currentDate.toLocaleDateString("en-US", {
+            weekday: "short",
+            month: "2-digit",
+            day: "2-digit",
+          })}
+        </Text>
+      </View>
     </View>
   );
 };
---
+++ components/dashboard/SubjectCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const SubjectCard = () => {
+  return (
+    <View>
+      <Text>SubjectCard</Text>
+    </View>
+  );
+};
+
+export default SubjectCard;
